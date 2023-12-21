f567a6f45bf41b928172133c4fe92ecb552c3014
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2023-12-21 01:59:03 -0300

    challenge 1 solved

---
+++ challenge-1/index.js
@@ -0,0 +1,8 @@
+console.time()
+const input = require("./input");
+
+const lineNumbersArr = input.replace(/[^0-9\n]+/g, '').split('\n')
+
+const total = lineNumbersArr.reduce((acum, value) => acum + parseInt(value[0] + value[value.length - 1]), 0);
+console.timeEnd()
+console.log(total)
